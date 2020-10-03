048e71fe09638bc59b73757eddd80a3ec694c96a
kalraarpit0003 <35924535+kalraarpit0003@users.noreply.github.com>
2020-10-03 14:02:33 +0530

    initial commit

---
+++ precache-manifest.9d92a85ce9250da927b865724ff3bddd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a81992490366d8d0a411dc8dd4c440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe86a91a0b3e73d02b86",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0c8d25b46c67218207be",
+    "url": "/static/js/2.017ca05d.chunk.js"
+  },
+  {
+    "revision": "7d79e8eaf32443c6fec2",
+    "url": "/static/js/3.87e06d43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/3.87e06d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219af553264a0d435833",
+    "url": "/static/js/4.f3bcd73b.chunk.js"
+  },
+  {
+    "revision": "fe86a91a0b3e73d02b86",
+    "url": "/static/js/main.fac0472f.chunk.js"
+  },
+  {
+    "revision": "d8383a392852d03a1d53",
+    "url": "/static/js/runtime-main.ba86d71e.js"
+  }
+]);
